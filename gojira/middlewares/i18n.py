52d5e8fda059eede99b3276a958cb084654bf846
Hitalo <hitalo331@outlook.com>
2023-04-21 19:39:02 -0300

    Feat: Uses group language over user language

--- gojira/middlewares/i18n.py
+++ gojira/middlewares/i18n.py
@@ -3,29 +3,39 @@
 
 from typing import Any, Dict, Optional, cast
 
-from aiogram.types import TelegramObject, User
+from aiogram.enums import ChatType
+from aiogram.types import Chat, TelegramObject, User
 from aiogram.utils.i18n import I18nMiddleware
 from babel import Locale, UnknownLocaleError
 
-from gojira.database.models import Users
+from gojira.database.models import Chats, Users
 
 
 class MyI18nMiddleware(I18nMiddleware):
     async def get_locale(self, event: TelegramObject, data: Dict[str, Any]) -> str:
-        user: Optional[User] | Optional[Users] = data.get("event_from_user", None)
+        user: Optional[User] = data.get("event_from_user")
+        chat: Optional[Chat] = data.get("event_chat")
 
-        if user is None or user.language_code is None:
+        if chat is not None and chat.type == ChatType.PRIVATE:
+            obj = user
+            objdb = Users
+        else:
+            obj = chat
+            objdb = Chats
+
+        if obj is None:
             return self.i18n.default_locale
 
-        user = await Users.get_or_none(id=user.id)
-        if user is None:
+        obj = await objdb.get_or_none(id=obj.id)
+        if obj is None:
             return self.i18n.default_locale
 
         try:
-            locale = Locale.parse(user.language_code, sep="-")
+            locale = Locale.parse(obj.language_code, sep="-")
         except UnknownLocaleError:
             return self.i18n.default_locale
 
         if locale.language not in self.i18n.available_locales:
             return self.i18n.default_locale
+
         return cast(str, locale.language)
