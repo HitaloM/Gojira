8b9a72693c2d9e297fbbec6c333c037a9d310e8d
Hitalo <hitalo331@outlook.com>
2023-05-24 22:58:47 -0300

    Feat: Adds missing commands in Telegram cmd list

--- gojira/utils/command_list.py
+++ gojira/utils/command_list.py
@@ -19,15 +19,7 @@ async def set_ui_commands(bot: Bot, i18n: I18n):
     with suppress(TelegramRetryAfter):
         await bot.delete_my_commands()
         for lang in i18n.available_locales:
-            user_commands: list[BotCommand] = [
-                BotCommand(
-                    command="start",
-                    description=_("Start the bot.", locale=lang),
-                ),
-                BotCommand(
-                    command="help",
-                    description=_("Get help.", locale=lang),
-                ),
+            all_chats_commands: list[BotCommand] = [
                 BotCommand(
                     command="anime",
                     description=_("Get anime informations.", locale=lang),
@@ -35,6 +27,22 @@ async def set_ui_commands(bot: Bot, i18n: I18n):
                 BotCommand(
                     command="manga",
                     description=_("Get manga informations.", locale=lang),
+                ),
+                BotCommand(
+                    command="character",
+                    description=_("Get character informations.", locale=lang),
+                ),
+                BotCommand(
+                    command="staff",
+                    description=_("Get staff informations.", locale=lang),
+                ),
+                BotCommand(
+                    command="studio",
+                    description=_("Get studio informations.", locale=lang),
+                ),
+                BotCommand(
+                    command="user",
+                    description=_("Get AniList user informations.", locale=lang),
                 ),
                 BotCommand(
                     command="language",
@@ -46,24 +54,20 @@ async def set_ui_commands(bot: Bot, i18n: I18n):
                 ),
             ]
 
-            group_commands: list[BotCommand] = [
+            user_commands: list[BotCommand] = [
                 BotCommand(
-                    command="anime",
-                    description=_("Get anime informations.", locale=lang),
+                    command="start",
+                    description=_("Start the bot.", locale=lang),
                 ),
                 BotCommand(
-                    command="manga",
-                    description=_("Get manga informations.", locale=lang),
+                    command="help",
+                    description=_("Get help.", locale=lang),
                 ),
-                BotCommand(
-                    command="language",
-                    description=_("Change bot language.", locale=lang),
-                ),
-                BotCommand(
-                    command="about",
-                    description=_("About the bot.", locale=lang),
-                ),
-            ]
+            ] + all_chats_commands
+
+            group_commands: list[
+                BotCommand
+            ] = all_chats_commands  # this will be changed in the future with new commands
 
             await bot.set_my_commands(
                 user_commands,
